fda85bf7417373d81b22e08ef77ba968ea9f3c81
uimarshall <uimarshall@gmail.com>
2020-05-19 15:09:58 +0100

    resolve file import error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@
 The index file holds all the different reducers*/
 import { combineReducers } from "redux";
 import productsReducer from "./productsReducer";
-import { errorReducer } from "./errorReducer";
+import errorReducer from "./errorReducer";
 
 export default combineReducers({
 	products: productsReducer,
