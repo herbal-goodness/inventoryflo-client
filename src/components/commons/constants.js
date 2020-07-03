529f658f400d85495bdfd81ffca01d0df69f3b4e
engreric <okemmadueric@gmail.com>
2020-07-03 19:12:27 +0100

    [clean up code]

---
+++ src/components/commons/constants.js
@@ -0,0 +1,66 @@
+export const FOOTER_CONTENT = [
+  {
+    title: "Products",
+    links: [
+      {
+        to: "/pricing",
+        text: "Pricing",
+      },
+      {
+        to: "/pricing",
+        text: "Features",
+      },
+    ],
+  },
+  {
+    title: "Integration",
+    links: [
+      {
+        to: "/shopify",
+        text: "Shopify",
+      },
+      {
+        to: "/amazon",
+        text: "Amazon",
+      },
+      {
+        to: "/amazon-fba",
+        text: "Amazon FBA",
+      },
+      {
+        to: "/efulfillment",
+        text: "eFulfillment",
+      },
+      {
+        to: "/google-drive",
+        text: "Google Drive",
+      },
+      {
+        to: "/slack",
+        text: "Slack",
+      },
+    ],
+  },
+
+  {
+    title: "Company",
+    links: [
+      {
+        to: "/about",
+        text: "About",
+      },
+      {
+        to: "/customer-service",
+        text: "Customer Service",
+      },
+      {
+        to: "/product-demo",
+        text: "Product demo",
+      },
+      {
+        to: "/contact-support",
+        text: "Contact Supprt",
+      },
+    ],
+  },
+];
