f90a6a53e98ef5c212de763aa6c91ac722fb96a2
engreric <okemmadueric@gmail.com>
2020-07-13 20:16:21 +0100

    [add] export functionality

--- src/components/warehouse/inventory/Sales.js
+++ src/components/warehouse/inventory/Sales.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import SalesTable from "./Table";
 import { makeData } from "../constants";
 import InventorySidePane from "./InventorySidePane";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { Button } from "react-bootstrap";
 
 function SalesContainer() {
+  const dispatch = useDispatch();
+  const [exportData, setExport] = useState(null);
+
+  const {
+    isLoading,
+    productsLoaded,
+    sales,
+    hasShopifyUrl,
+    hasShopifySecret,
+    isSuccessful,
+  } = useSelector(
+    ({ sales, userInfo }) => ({
+      hasShopifyUrl:
+        userInfo.user.shopifyDomain && userInfo.user.shopifyDomain.length > 3,
+      hasShopifySecret:
+        userInfo.user.shopifySecret && userInfo.user.shopifySecret.length > 3,
+      isSuccessful: userInfo.successful,
+      sales: sales.products,
+      isLoading: sales.loading,
+      productsLoaded: sales.successful,
+    }),
+    shallowEqual
+  );
+  useEffect(() => {
+    hasShopifyUrl &&
+      hasShopifySecret &&
+      isSuccessful &&
+      sales === null &&
+      dispatch({ type: "GET_PRODUCTS" });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (isLoading) {
+        dispatch({
+          type: "PRODUCTS_ERROR",
+        });
+      }
+    }, 5000);
+  }, []);
+
+  const exportFile = () => {
+    exportData.save();
+  };
+
   return (
     <div className="container-fluid mx-auto main">
       <div className="row">
@@ -19,16 +66,21 @@ function SalesContainer() {
             </h2>
             <h4>
               <Link to="" className="btn btn-outline-primary mr-2">
-                <i className="fa fa-sign-out fa-fw mr-1" aria-hidden="true"></i>{" "}
+                <i className="fa fa-sign-out fa-fw mr-1" aria-hidden="true"></i>
                 Import from CSV
               </Link>
-              <Link to="" className="btn btn-outline-primary">
-                <i className="fa fa-sign-out fa-fw mr-1" aria-hidden="true"></i>{" "}
-                Export
-              </Link>
             </h4>
+            <Button onClick={exportFile} className="btn btn-outline-primary">
+              <i className="fa fa-sign-out fa-fw mr-1" aria-hidden="true"></i>
+              Export
+            </Button>
           </header>
-          <SalesTable />
+
+          <SalesTable
+            setExport={setExport}
+            isLoading={isLoading}
+            sales={sales}
+          />
         </div>
       </div>
     </div>
--- src/components/warehouse/inventory/Table.js
+++ src/components/warehouse/inventory/Table.js
@@ -1,125 +1,89 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "@progress/kendo-theme-default/dist/all.css";
 import { withState, ColumnMenu } from "./withState";
 import { GridColumn, Grid } from "@progress/kendo-react-grid";
-import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import { ExcelExport } from "@progress/kendo-react-excel-export";
 import { Spinner } from "../../utils/components";
 
 const StatefulGrid = withState(Grid);
 
-const Table = () => {
-  const dispatch = useDispatch();
-
-  const {
-    isLoading,
-    productsLoaded,
-    sales,
-    hasShopifyUrl,
-    hasShopifySecret,
-    isSuccessful,
-  } = useSelector(
-    ({ sales, userInfo }) => ({
-      hasShopifyUrl:
-        userInfo.user.shopifyDomain && userInfo.user.shopifyDomain.length > 3,
-      hasShopifySecret:
-        userInfo.user.shopifySecret && userInfo.user.shopifySecret.length > 3,
-      isSuccessful: userInfo.successful,
-      sales: sales.products,
-      isLoading: sales.loading,
-      productsLoaded: sales.successful,
-    }),
-    shallowEqual
-  );
-  useEffect(() => {
-    hasShopifyUrl &&
-      hasShopifySecret &&
-      isSuccessful &&
-      sales === null &&
-      dispatch({ type: "GET_PRODUCTS" });
-  }, []);
-
-  setTimeout(() => {
-    if (isLoading) {
-      dispatch({
-        type: "PRODUCTS_ERROR",
-      });
-    }
-  }, 10000);
-
+const Table = ({ isLoading, sales, setExport }) => {
   return isLoading ? (
     <Spinner />
   ) : (
     <div>
-      <StatefulGrid data={sales || []} style={{ height: "600px" }}>
-        <GridColumn
-          field="image"
-          title={" "}
-          cell={(props) => (
-            <td colSpan={props.colSpan} style={props.style}>
-              {<img src={props.dataItem?.image} alt="" />}
-            </td>
-          )}
-          filterable={false}
-          sortable={false}
-          width={50}
-        />
-        <GridColumn
-          field="title"
-          title="Product Name"
-          width={200}
-          className="truncate"
-        />
-        <GridColumn
-          field="sku"
-          title="SKU"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="inventory_management"
-          title="Condition"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="fulfillment_service"
-          title="Location"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="inventory_policy"
-          title="Bin Location"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="inventory_quantity"
-          title="Available"
-          filter="numeric"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="vendor"
-          title="On Hand"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="price"
-          title="Price"
-          filter="numeric"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-        <GridColumn
-          field="updated_at"
-          title="Last Modified"
-          filterable={false}
-          columnMenu={ColumnMenu}
-        />
-      </StatefulGrid>
+      <ExcelExport data={sales} ref={(exporter) => setExport(exporter)}>
+        <StatefulGrid data={sales || []} style={{ height: "600px" }}>
+          <GridColumn
+            field="image"
+            title={" "}
+            cell={(props) => (
+              <td colSpan={props.colSpan} style={props.style}>
+                {<img src={props.dataItem?.image} alt="" />}
+              </td>
+            )}
+            filterable={false}
+            sortable={false}
+            width={50}
+          />
+          <GridColumn
+            field="title"
+            title="Product Name"
+            width={200}
+            className="truncate"
+          />
+          <GridColumn
+            field="sku"
+            title="SKU"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="inventory_management"
+            title="Condition"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="fulfillment_service"
+            title="Location"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="inventory_policy"
+            title="Bin Location"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="inventory_quantity"
+            title="Available"
+            filter="numeric"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="vendor"
+            title="On Hand"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="price"
+            title="Price"
+            filter="numeric"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+          <GridColumn
+            field="updated_at"
+            title="Last Modified"
+            filterable={false}
+            columnMenu={ColumnMenu}
+          />
+        </StatefulGrid>
+      </ExcelExport>
     </div>
   );
 };
