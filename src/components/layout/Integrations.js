55454fad745fd1279d40c9be70d5efeb7713a8ab
uimarshall <uimarshall@gmail.com>
2020-07-15 17:44:59 +0100

    Fix routing errors
    The fix includes:
    Links to all the integrations channel
    Footer links
    Favicon change.

--- src/components/layout/Integrations.js
+++ src/components/layout/Integrations.js
@@ -22,7 +22,8 @@ const Integrations = () => {
 					<div className="row mb-5">
 						<div className="col-lg-4 col-md-6">
 							<Link
-								to="https://www.shopify.com/"
+								to="/shopify"
+								target="_blank"
 								className="text-decoration-none">
 								<Cards
 									source={shopify}
@@ -39,104 +40,135 @@ const Integrations = () => {
 						</div>
 
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								className="comingSoon"
-								source={amazon}
-								alternate="amazon"
-								title="amazon"
-								header="amazon">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Stay on top of sales orders and fulfilments and have stock
-									levels updated in real-time with the Amazon integration.
-								</p>
-							</Cards>
+							<Link
+								to="/amazon"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									className="comingSoon"
+									source={amazon}
+									alternate="amazon"
+									title="amazon"
+									header="amazon">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Stay on top of sales orders and fulfilments and have stock
+										levels updated in real-time with the Amazon integration.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								source={walmart}
-								alternate="walmart"
-								title="walmart"
-								header="walmart">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Manage the products and listings in the largest retailer in
-									the world with over 80 million unique visitors.
-								</p>
-							</Cards>
+							<Link
+								to="/walmart"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									source={walmart}
+									alternate="walmart"
+									title="walmart"
+									header="walmart">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Manage the products and listings in the largest retailer in
+										the world with over 80 million unique visitors.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 					</div>
 					<SectionTitle title="Shipping service integrations" />
 					<div className="row mb-5">
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								source={amazonfba}
-								alternate="amazon FBA"
-								title="amazon FBA"
-								header="amazon FBA">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Inventoryflo synchronizes with the Amazon FBA so your stock
-									levels will be accurate across all your warehouse locations.
-								</p>
-							</Cards>
+							<Link
+								to="/amazon-fba"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									source={amazonfba}
+									alternate="amazon FBA"
+									title="amazon FBA"
+									header="amazon FBA">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Inventoryflo synchronizes with the Amazon FBA so your stock
+										levels will be accurate across all your warehouse locations.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								source={efulfill}
-								alternate="eFulfillment Service"
-								title="eFulfillment Service"
-								header="eFulfillment Service">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Inventoryflo synchronizes with the EFS to precisely track all
-									the stocks and provides a monitor to check on your
-									supplychain.
-								</p>
-							</Cards>
+							<Link
+								to="/efulfillment"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									source={efulfill}
+									alternate="eFulfillment Service"
+									title="eFulfillment Service"
+									header="eFulfillment Service">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Inventoryflo synchronizes with the EFS to precisely track
+										all the stocks and provides a monitor to check on your
+										supplychain.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 					</div>
 					<SectionTitle title="Other service integrations" />
 					<div className="row mb-5">
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								source={googledrive}
-								alternate="google drive"
-								title="google drive"
-								header="google drive">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Inventoryflo reports can be saved to the Google drive. Export
-									manual batch sheets or files to visualize inventory and
-									supplychain data.
-								</p>
-							</Cards>
+							<Link
+								to="/google-drive"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									source={googledrive}
+									alternate="google drive"
+									title="google drive"
+									header="google drive">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Inventoryflo reports can be saved to the Google drive.
+										Export manual batch sheets or files to visualize inventory
+										and supplychain data.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 
 						<div className="col-lg-4 col-md-6">
-							<Cards
-								source={slack}
-								alternate="slack"
-								title="slack"
-								header="slack">
-								<h3 className="ribbon">
-									<span>Coming Soon!!!</span>
-								</h3>
-								<p className="card-text text-muted">
-									Bring your team together by posting updates from inventoryflo
-									to slack and update the organization with latest content.
-								</p>
-							</Cards>
+							<Link
+								to="/slack"
+								target="_blank"
+								className="text-decoration-none">
+								<Cards
+									source={slack}
+									alternate="slack"
+									title="slack"
+									header="slack">
+									<h3 className="ribbon">
+										<span>Coming Soon!!!</span>
+									</h3>
+									<p className="card-text text-muted">
+										Bring your team together by posting updates from
+										inventoryflo to slack and update the organization with
+										latest content.
+									</p>
+								</Cards>
+							</Link>
 						</div>
 					</div>
 				</div>
