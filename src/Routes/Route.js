a1d725abaa36712bedd6c576f75ec9ae71f76fdd
engreric <okemmadueric@gmail.com>
2020-07-16 11:39:21 +0100

    code clean up

--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -11,104 +11,32 @@ import PrivateRoute from "./PrivateRoutes";
 import { authRoutes, privateRoutes } from "./constants";
 
 function Inventoryflo() {
-	return (
-		<Router>
-			<Layout>
-				<Suspense
-					fallback={
-						<h1 style={{ margin: "20% 22%" }}>
-							<Spinner />
-						</h1>
-					}>
-					<Switch>
-						{authRoutes.map((item, i) => (
-							<AuthRoute key={i} exact {...item} />
-						))}
-						{privateRoutes.map((item, i) => (
-							<PrivateRoute key={i} exact {...item} />
-						))}
-						<Route exact path="/" component={Landing} />
-						<Route exact path="/privacy-policy" component={Policy} />
-						<Route exact path="/integrations" component={Integrations} />
-						<Route
-							path="/shopify"
-							component={() => {
-								window.location.href = "https://www.shopify.com/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/amazon"
-							component={() => {
-								window.location.href = "https://www.amazon.com/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/amazon-fba"
-							component={() => {
-								window.location.href =
-									"https://sell.amazon.com/fulfillment-by-amazon.html";
-								return null;
-							}}
-						/>
-						<Route
-							path="/efulfillment"
-							component={() => {
-								window.location.href = "https://www.efulfillmentservice.com/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/walmart"
-							component={() => {
-								window.location.href = "https://www.walmart.com/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/slack"
-							component={() => {
-								window.location.href = "https://slack.com/intl/en-ng/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/google-drive"
-							component={() => {
-								window.location.href = "https://www.google.com/drive/";
-								return null;
-							}}
-						/>
-						<Route
-							path="/info"
-							component={() => {
-								window.location.href = "info@inventoryflo.com";
-								return null;
-							}}
-						/>
-						<Route
-							path="/truste"
-							component={() => {
-								window.location.href =
-									"https://feedback-form.truste.com/watchdog/request";
-								return null;
-							}}
-						/>
-						<Route
-							path="/do-not-track"
-							component={() => {
-								window.location.href = "https://allaboutdnt.com/";
-								return null;
-							}}
-						/>
-
-						<Route exact path="*" component={NotFound} />
-					</Switch>
-				</Suspense>
-			</Layout>
-		</Router>
-	);
+  return (
+    <Router>
+      <Layout>
+        <Suspense
+          fallback={
+            <h1 style={{ margin: "20% 22%" }}>
+              <Spinner />
+            </h1>
+          }
+        >
+          <Switch>
+            {authRoutes.map((item, i) => (
+              <AuthRoute key={i} exact {...item} />
+            ))}
+            {privateRoutes.map((item, i) => (
+              <PrivateRoute key={i} exact {...item} />
+            ))}
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/privacy-policy" component={Policy} />
+            <Route exact path="/integrations" component={Integrations} />
+            <Route exact path="*" component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Layout>
+    </Router>
+  );
 }
 
 export default Inventoryflo;
--- src/components/layout/Integrations.js
+++ src/components/layout/Integrations.js
@@ -11,170 +11,191 @@ import Cards from "../commons/Cards";
 import SectionTitle from "../commons/SectionTitle";
 
 const Integrations = () => {
-	return (
-		<div>
-			<div className="integrations">
-				<header className="integrations-header">
-					<h1 className="text-capitalize text-center">integrations</h1>
-				</header>
-				<div className="container mt-5" id="integrations-container">
-					<SectionTitle title="eCommerce" />
-					<div className="row mb-5">
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/shopify"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={shopify}
-									alternate="shopify"
-									title="shopify"
-									header="shopify">
-									<p className="card-text text-muted">
-										Single place to track all your orders, stock levels,
-										customer contacts, product details/images and more. Manage
-										orders and inventory.
-									</p>
-								</Cards>
-							</Link>
-						</div>
+  return (
+    <div>
+      <div className="integrations">
+        <header className="integrations-header">
+          <h1 className="text-capitalize text-center">integrations</h1>
+        </header>
+        <div className="container mt-5" id="integrations-container">
+          <SectionTitle title="eCommerce" />
+          <div className="row mb-5">
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://www.shopify.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={shopify}
+                  alternate="shopify"
+                  title="shopify"
+                  header="shopify"
+                >
+                  <p className="card-text text-muted">
+                    Single place to track all your orders, stock levels,
+                    customer contacts, product details/images and more. Manage
+                    orders and inventory.
+                  </p>
+                </Cards>
+              </a>
+            </div>
 
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/amazon"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									className="comingSoon"
-									source={amazon}
-									alternate="amazon"
-									title="amazon"
-									header="amazon">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Stay on top of sales orders and fulfilments and have stock
-										levels updated in real-time with the Amazon integration.
-									</p>
-								</Cards>
-							</Link>
-						</div>
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/walmart"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={walmart}
-									alternate="walmart"
-									title="walmart"
-									header="walmart">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Manage the products and listings in the largest retailer in
-										the world with over 80 million unique visitors.
-									</p>
-								</Cards>
-							</Link>
-						</div>
-					</div>
-					<SectionTitle title="Shipping service integrations" />
-					<div className="row mb-5">
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/amazon-fba"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={amazonfba}
-									alternate="amazon FBA"
-									title="amazon FBA"
-									header="amazon FBA">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Inventoryflo synchronizes with the Amazon FBA so your stock
-										levels will be accurate across all your warehouse locations.
-									</p>
-								</Cards>
-							</Link>
-						</div>
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/efulfillment"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={efulfill}
-									alternate="eFulfillment Service"
-									title="eFulfillment Service"
-									header="eFulfillment Service">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Inventoryflo synchronizes with the EFS to precisely track
-										all the stocks and provides a monitor to check on your
-										supplychain.
-									</p>
-								</Cards>
-							</Link>
-						</div>
-					</div>
-					<SectionTitle title="Other service integrations" />
-					<div className="row mb-5">
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/google-drive"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={googledrive}
-									alternate="google drive"
-									title="google drive"
-									header="google drive">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Inventoryflo reports can be saved to the Google drive.
-										Export manual batch sheets or files to visualize inventory
-										and supplychain data.
-									</p>
-								</Cards>
-							</Link>
-						</div>
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://www.amazon.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  className="comingSoon"
+                  source={amazon}
+                  alternate="amazon"
+                  title="amazon"
+                  header="amazon"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Stay on top of sales orders and fulfilments and have stock
+                    levels updated in real-time with the Amazon integration.
+                  </p>
+                </Cards>
+              </a>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://www.walmart.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={walmart}
+                  alternate="walmart"
+                  title="walmart"
+                  header="walmart"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Manage the products and listings in the largest retailer in
+                    the world with over 80 million unique visitors.
+                  </p>
+                </Cards>
+              </a>
+            </div>
+          </div>
+          <SectionTitle title="Shipping service integrations" />
+          <div className="row mb-5">
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://sell.amazon.com/fulfillment-by-amazon.html"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={amazonfba}
+                  alternate="amazon FBA"
+                  title="amazon FBA"
+                  header="amazon FBA"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Inventoryflo synchronizes with the Amazon FBA so your stock
+                    levels will be accurate across all your warehouse locations.
+                  </p>
+                </Cards>
+              </a>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://www.efulfillmentservice.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={efulfill}
+                  alternate="eFulfillment Service"
+                  title="eFulfillment Service"
+                  header="eFulfillment Service"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Inventoryflo synchronizes with the EFS to precisely track
+                    all the stocks and provides a monitor to check on your
+                    supplychain.
+                  </p>
+                </Cards>
+              </a>
+            </div>
+          </div>
+          <SectionTitle title="Other service integrations" />
+          <div className="row mb-5">
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://www.google.com/drive/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={googledrive}
+                  alternate="google drive"
+                  title="google drive"
+                  header="google drive"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Inventoryflo reports can be saved to the Google drive.
+                    Export manual batch sheets or files to visualize inventory
+                    and supplychain data.
+                  </p>
+                </Cards>
+              </a>
+            </div>
 
-						<div className="col-lg-4 col-md-6">
-							<Link
-								to="/slack"
-								target="_blank"
-								className="text-decoration-none">
-								<Cards
-									source={slack}
-									alternate="slack"
-									title="slack"
-									header="slack">
-									<h3 className="ribbon">
-										<span>Coming Soon!!!</span>
-									</h3>
-									<p className="card-text text-muted">
-										Bring your team together by posting updates from
-										inventoryflo to slack and update the organization with
-										latest content.
-									</p>
-								</Cards>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+            <div className="col-lg-4 col-md-6">
+              <a
+                href="https://slack.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-decoration-none"
+              >
+                <Cards
+                  source={slack}
+                  alternate="slack"
+                  title="slack"
+                  header="slack"
+                >
+                  <h3 className="ribbon">
+                    <span>Coming Soon!!!</span>
+                  </h3>
+                  <p className="card-text text-muted">
+                    Bring your team together by posting updates from
+                    inventoryflo to slack and update the organization with
+                    latest content.
+                  </p>
+                </Cards>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Integrations;
