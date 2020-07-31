648eb67c0948be268d186be9cb98f46961baf81b
uimarshall <uimarshall@gmail.com>
2020-07-31 15:46:16 +0100

    fix orders page empty space and change link colors on hover

--- src/components/warehouse/inventory/Orders.js
+++ src/components/warehouse/inventory/Orders.js
@@ -6,73 +6,72 @@ import { Button } from "react-bootstrap";
 import OrdersSidePane from "./OrdersSidePane";
 
 function OrdersContainer() {
-  const dispatch = useDispatch();
-  const [exportData, setExport] = useState(null);
-  const [query, setQuery] = useState("");
-  const [filterChannel, setFilter] = useState({});
+	const dispatch = useDispatch();
+	const [exportData, setExport] = useState(null);
+	const [query, setQuery] = useState("");
+	const [filterChannel, setFilter] = useState({});
 
-  const {
-    isLoading,
-    orders,
-    hasShopifyUrl,
-    hasShopifySecret,
-    isSuccessful,
-  } = useSelector(
-    ({ orders, userInfo }) => ({
-      hasShopifyUrl:
-        userInfo.user.shopifyDomain && userInfo.user.shopifyDomain.length > 3,
-      hasShopifySecret:
-        userInfo.user.shopifySecret && userInfo.user.shopifySecret.length > 3,
-      isSuccessful: userInfo.successful,
-      orders: orders.userOrders,
-      isLoading: orders.loading,
-    }),
-    shallowEqual
-  );
-  const handleSearch = (e) => {
-    e.preventDefault();
-    const { value } = e.target;
-    setQuery(value);
-  };
+	const {
+		isLoading,
+		orders,
+		hasShopifyUrl,
+		hasShopifySecret,
+		isSuccessful,
+	} = useSelector(
+		({ orders, userInfo }) => ({
+			hasShopifyUrl:
+				userInfo.user.shopifyDomain && userInfo.user.shopifyDomain.length > 3,
+			hasShopifySecret:
+				userInfo.user.shopifySecret && userInfo.user.shopifySecret.length > 3,
+			isSuccessful: userInfo.successful,
+			orders: orders.userOrders,
+			isLoading: orders.loading,
+		}),
+		shallowEqual
+	);
+	const handleSearch = (e) => {
+		e.preventDefault();
+		const { value } = e.target;
+		setQuery(value);
+	};
 
+	const handleChange = (e) => {
+		e.preventDefault();
+		const { value, name } = e.target;
+		setFilter({ ...filterChannel, [name]: value });
+	};
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    const { value, name } = e.target;
-    setFilter({ ...filterChannel, [name]: value });
-  };
+	useEffect(() => {
+		hasShopifyUrl &&
+			hasShopifySecret &&
+			isSuccessful &&
+			(orders === null || orders.length < 40) &&
+			dispatch({ type: "GET_ORDERS", payload: {} });
+	}, []);
 
-  useEffect(() => {
-    hasShopifyUrl &&
-      hasShopifySecret &&
-      isSuccessful &&
-      (orders === null || orders.length < 40) &&
-      dispatch({ type: "GET_ORDERS", payload: {} });
-  }, []);
+	const exportFile = () => {
+		setFilter({});
+		exportData.save();
+	};
 
-  const exportFile = () => {
-    setFilter({});
-    exportData.save();
-  };
+	const clearFilter = () => {
+		dispatch({ type: "GET_ORDERS", payload: {} });
+	};
 
-  const clearFilter = () => {
-    dispatch({ type: "GET_ORDERS", payload: {} });
-  };
-
-  return (
-    <div className="container-fluid mx-auto main">
-      <div className="row">
-        <div className="col-md-3 inv-side-wrapper pt-5 inv-col-1">
-          <h2 className="filter-inv-header">filter inventory</h2>
-          <OrdersSidePane
-            handleChange={handleChange}
-            handleSearch={handleSearch}
-            type="order"
-            title="Orders"
-          />
-        </div>
-        <div className="col-md-9 inv-col-2">
-          <header className="d-flex justify-content-between mb-2 dashboard-header flex-wrap">
+	return (
+		<div className="container-fluid mx-auto">
+			<div className="row">
+				<div className="col-md-3 inv-side-wrapper pt-5 inv-col-1">
+					<h2 className="filter-inv-header">filter inventory</h2>
+					<OrdersSidePane
+						handleChange={handleChange}
+						handleSearch={handleSearch}
+						type="order"
+						title="Orders"
+					/>
+				</div>
+				<div className="col-md-9 inv-col-2">
+					<header className="d-flex justify-content-between mb-2 dashboard-header flex-wrap">
 						<div>
 							<h2>
 								<i class="fa fa-shopping-cart fa-fw" aria-hidden="true"></i>
@@ -110,19 +109,18 @@ function OrdersContainer() {
 						</div>
 					</header>
 
-          <SalesTable
-            setExport={setExport}
-            isLoading={isLoading}
-            orders={orders}
-            query={query}
-            clearFilter={clearFilter}
-            filterChannel={filterChannel}
-          />
-        </div>
-      </div>
-    </div>
-  );
-
+					<SalesTable
+						setExport={setExport}
+						isLoading={isLoading}
+						orders={orders}
+						query={query}
+						clearFilter={clearFilter}
+						filterChannel={filterChannel}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default OrdersContainer;
