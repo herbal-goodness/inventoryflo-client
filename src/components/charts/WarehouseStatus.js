54e337c770beb83ebd1daae0149c4874dc9110ef
afams val <myceet1@gmail.com>
2020-08-11 10:19:17 +0100

    updated hook

--- src/components/charts/WarehouseStatus.js
+++ src/components/charts/WarehouseStatus.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Bar } from "react-chartjs-2";
 
-const state = {
-  labels: ["Warehouse 1", "Warehouse 2", "Warehouse 3"],
-  datasets: [
-    {
-      label: "Available",
-      backgroundColor: "#20846B",
-      borderColor: "green",
-      borderWidth: 1,
-      data: [65, 159, 80],
-    },
-    {
-      label: "Out of stock",
-      backgroundColor: "#F7931E",
-      borderColor: "rgba(0,0,0,1)",
-      borderWidth: 1,
-      data: [65, 59, 80],
-    },
-  ],
-};
-
 const WarehouseStatus = () => {
+  const [state, setState] = useState({
+    labels: ["Warehouse 1", "Warehouse 2", "Warehouse 3"],
+    datasets: [
+      {
+        label: "Available",
+        backgroundColor: "#20846B",
+        borderColor: "green",
+        borderWidth: 1,
+        data: [65, 159, 80],
+      },
+      {
+        label: "Out of stock",
+        backgroundColor: "#F7931E",
+        borderColor: "rgba(0,0,0,1)",
+        borderWidth: 1,
+        data: [65, 59, 80],
+      },
+    ],
+  });
+
   return (
     <div>
       <Bar
