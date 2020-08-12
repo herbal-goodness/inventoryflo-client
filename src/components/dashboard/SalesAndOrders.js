52c6af6ddd05ace9ee53cd065a334457aad2ec1a
afams val <myceet1@gmail.com>
2020-08-12 15:18:50 +0100

    updated dashboard

--- src/components/dashboard/SalesAndOrders.js
+++ src/components/dashboard/SalesAndOrders.js
@@ -26,7 +26,7 @@ const SalesAndOrders = () => {
         <div className="row">
           <div className="col-12">
             <div className="box-1 mt-3 py-3 text-center">
-              <span className="total-order-1">Total Orders </span>
+              <span className="total-order-1 py-2">Total Orders </span>
               <span className="total-order-3 text-green ml-4">60 </span>
               <span className="total-order-1">
                 <i className="fa fa-arrow-up text-green fa-lg"></i>
--- src/components/dashboard/SalesBoxOne.js
+++ src/components/dashboard/SalesBoxOne.js
@@ -2,15 +2,15 @@ import React from "react";
 const SalesBoxOne = ({ title, value, centage }) => {
   return (
     <div className="col-6">
-      <div className="box-1 mt-3  py-1 text-center">
+      <div className="box-1 mt-3  py-3 text-center">
         <div>
-          <span className="total-order-2 text-green">{value} </span>
+          <span className="total-order-2 py-3 text-green">{value} </span>
           <span className="total-order-0">
             <i className="fa fa-arrow-up text-green fa-lg"></i>
           </span>
           <sub>{centage}%</sub>
         </div>
-        <small className="my-1">{title}</small>
+        <small className="my-1 ">{title}</small>
       </div>
     </div>
   );
